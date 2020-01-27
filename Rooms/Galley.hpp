5acaf796f532759d36599713129d20f420276e68
ryanalcorn88 <39017022+ryanalcorn88@users.noreply.github.com>
2020-01-26 20:46:49 -0800

    Create Galley.hpp

---
+++ Rooms/Galley.hpp
@@ -0,0 +1,29 @@
+/* Project Name: Capstone Project
+   Class: CS 467
+   Team: Team Thing- Ryan Alcorn, Joel Alfaro, Neil Gayeta
+   File: Galley.hpp
+   Description: Class header for Galley room.*/
+
+#pragma once
+
+#include "Room.hpp"
+
+class Galley : public Room {
+public:
+	std::string getName();
+	std::string getLongDescrip();
+	std::string getShortDescrip();
+	std::string getExitLong();
+	std::string getExitShort();
+	bool getFeatureOneHap();
+	bool getFeatureTwoHap();
+	void featureOne();
+	void featureTwo();
+	void setRoom();
+	void itemsInRoom();
+	void addItem(Item*);
+	void removeItem(Item*);
+	void displayDescrip();
+	void displayExits();
+	Room* changeRooms(std::vector<Room*>, std::string);
+};
