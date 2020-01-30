6e3d882fb45be692d2ad26fab71b5e9433a94430
ryanalcorn88 <39017022+ryanalcorn88@users.noreply.github.com>
2020-01-29 19:31:22 -0800

    Update SickBay.cpp

--- Rooms/SickBay.cpp
+++ Rooms/SickBay.cpp
@@ -64,37 +64,63 @@ bool SickBay::getFeatureTwoHap()
 }
 
 /* Function performs the action for the first feature of the room.*/
-void SickBay::featureOne()
+int SickBay::featureOne(std::vector<Item*> inventory)
 {
+	int outcome = 0;
+
 	std::cout << "Player inspects the toilet paper." << std::endl;
 
 	++fOneHappened;
+
+	return outcome;
 }
 
 /* Function performs the action for the second feature of the room.*/
-void SickBay::featureTwo()
+int SickBay::featureTwo(std::vector<Item*> inventory)
 {
+	int outcome = 0;
+
 	std::cout << "Player uses the bathroom." << std::endl;
 
 	++fTwoHappened;
+
+	return outcome;
 }
 
-/* Function adds an item to the vector of items in the room.*/
-void SickBay::addItem(Item* newItem)
+/* Function adds an item to the vector of items in the room. It receives the player's inventory
+* in case the player wishes to drop an item from their inventory into the room. */
+void SickBay::addItem(Item* newItem, std::vector<Item*> inventory, int number)
 {
-	items.push_back(newItem);
-	newItem->setLocation(name);
+	if (number == 0)
+	{
+		items.push_back(newItem);
+		newItem->setLocation(name);
+	}
+	else
+	{
+		items.push_back(newItem);
+
+		for (unsigned int i = 0; i < inventory.size(); ++i)
+		{
+			if (inventory[i]->getName() == newItem->getName())
+			{
+				inventory.erase(inventory.begin() + i);
+				newItem->setLocation(name);
+			}
+		}
+	}
 }
 
-/* Function removes an item from the vector of items in the room.*/
-void SickBay::removeItem(Item* removeItem)
+/* Function removes an item from the vector of items in the room and adds it to the player's inventory.*/
+void SickBay::removeItem(Item* removeItem, std::vector<Item*> inventory)
 {
 	if (items.size() > 0)
 	{
-		for (int i = 0; i < items.size(); ++i)
+		for (unsigned int i = 0; i < items.size(); ++i)
 		{
 			if (items[i]->getName() == removeItem->getName())
 			{
+				inventory.push_back(removeItem);
 				items.erase(items.begin() + i);
 				removeItem->setLocation("player's inventory");
 			}
@@ -108,7 +134,7 @@ void SickBay::itemsInRoom()
 	if (items.size() > 0)
 	{
 		std::cout << "The current items in the " << getName() << " are: ";
-		for (int i = 0; i < items.size(); i++)
+		for (unsigned int i = 0; i < items.size(); i++)
 		{
 			if (i != (items.size() - 1))
 			{
