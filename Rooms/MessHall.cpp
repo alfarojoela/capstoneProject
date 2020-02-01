d54eb1af1f100a483a9a3335a6607dd2668664a3
ryanalcorn88 <39017022+ryanalcorn88@users.noreply.github.com>
2020-02-01 09:48:23 -0800

    Update MessHall.cpp

--- Rooms/MessHall.cpp
+++ Rooms/MessHall.cpp
@@ -11,8 +11,8 @@ void MessHall::setRoom()
 {
 	name = "messHall";
 	longDes = "You enter the mess hall. This is the area of the base where the crew would gather to share their meals together\n"
-		" and share stories. This is one of the larger rooms in the base considering it has to seat all of the crew members.\n"
-		" You notice what looks like an untouched meal on one of the tables. There's also a bottle of booze on another table.\n";
+		"and share stories. This is one of the larger rooms in the base considering it has to seat all of the crew members.\n"
+		"You notice what looks like an untouched meal on one of the tables. There's also a bottle of booze on another table.\n";
 	shortDes = "You return to the mess hall. The room where the crew ate their meals together.\n";
 	exitLong = "A hallway which connects to many rooms in the base including the conference room and garage is east of you.\n"
 		"The galley is also south of your location.";
@@ -20,48 +20,6 @@ void MessHall::setRoom()
 	fOneHappened = 0;
 	fTwoHappened = 0;
 	roomEntered = 0;
-}
-
-/* Function returns the name of the room. */
-std::string MessHall::getName()
-{
-	return name;
-}
-
-/* Function returns the long description of the room. */
-std::string MessHall::getLongDescrip()
-{
-	return longDes;
-}
-
-/* Function returns the short description of the room. */
-std::string MessHall::getShortDescrip()
-{
-	return shortDes;
-}
-
-/* Function returns the long version of the exits description of the room. */
-std::string MessHall::getExitLong()
-{
-	return exitLong;
-}
-
-/* Function returns the short version of the exits description of the room. */
-std::string MessHall::getExitShort()
-{
-	return exitShort;
-}
-
-/* Function returns the boolean value that represents whether a feature happened.*/
-bool MessHall::getFeatureOneHap()
-{
-	return fOneHappened;
-}
-
-/* Function returns the boolean value that represents whether a feature happened.*/
-bool MessHall::getFeatureTwoHap()
-{
-	return fTwoHappened;
 }
 
 /* Function performs the action for the first feature of the room.*/
@@ -86,92 +44,6 @@ int MessHall::featureTwo(Player* user)
 	++fTwoHappened;
 
 	return outcome;
-}
-
-/* Function adds an item to the vector of items in the room. It receives a Player instance
-* in case the player wishes to drop an item from their inventory into the room. */
-void MessHall::addItem(Item* newItem, Player* user, int number)
-{
-	if (number == 0)
-	{
-		items.push_back(newItem);
-		newItem->setLocation(name);
-	}
-	else
-	{
-		items.push_back(newItem);
-
-		//Call Player function to remove item from player's inventory
-	}
-}
-
-/* Function removes an item from the vector of items in the room and adds it to the player's inventory.*/
-void MessHall::removeItem(Item* removeItem, Player* user)
-{
-	if (items.size() > 0)
-	{
-		for (unsigned int i = 0; i < items.size(); ++i)
-		{
-			if (items[i]->getName() == removeItem->getName())
-			{
-				user->setInventory(removeItem);
-				items.erase(items.begin() + i);
-				removeItem->setLocation("player's inventory");
-			}
-		}
-	}
-}
-
-/* Function displays the items currently in the room.*/
-void MessHall::itemsInRoom()
-{
-	if (items.size() > 0)
-	{
-		std::cout << "The current items in the " << getName() << " are: ";
-		for (unsigned int i = 0; i < items.size(); i++)
-		{
-			if (i != (items.size() - 1))
-			{
-				std::cout << items[i]->getName() << ", ";
-			}
-			else
-			{
-				std::cout << items[i]->getName() << std::endl;
-				std::cout << std::endl;
-			}
-		}
-	}
-	else
-	{
-		std::cout << "There are no items to pick up in the room." << std::endl;
-	}
-}
-
-/* Function displays either the short or long description based off a boolean value.*/
-void MessHall::displayDescrip()
-{
-	if (roomEntered == 0)
-	{
-		std::cout << longDes << std::endl;
-	}
-	else
-	{
-		std::cout << shortDes << std::endl;
-	}
-	++roomEntered;
-}
-
-/* Function displays the exit descriptions based on whether it is the first time the user entered the room.*/
-void MessHall::displayExits()
-{
-	if (roomEntered == 1)
-	{
-		std::cout << exitLong << std::endl;
-	}
-	else
-	{
-		std::cout << exitShort << std::endl;
-	}
 }
 
 /* Function receives the list of rooms and a phrase from the user. It then selects a room to move to based on
