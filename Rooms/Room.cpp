929ed37f43a3e76b4751fd0b6e2f90d979fd3db8
ryanalcorn88 <39017022+ryanalcorn88@users.noreply.github.com>
2020-02-01 09:37:12 -0800

    Create Room.cpp

---
+++ Rooms/Room.cpp
@@ -0,0 +1,129 @@
+#include "Room.hpp"
+
+/* Function returns the name of the room. */
+std::string Room::getName()
+{
+	return name;
+}
+
+/* Function returns the long description of the room. */
+std::string Room::getLongDescrip()
+{
+	return longDes;
+}
+
+/* Function returns the short description of the room. */
+std::string Room::getShortDescrip()
+{
+	return shortDes;
+}
+
+/* Function returns the long version of the exits description of the room. */
+std::string Room::getExitLong()
+{
+	return exitLong;
+}
+
+/* Function returns the short version of the exits description of the room. */
+std::string Room::getExitShort()
+{
+	return exitShort;
+}
+
+/* Function returns the boolean value that represents whether a feature happened.*/
+bool Room::getFeatureOneHap()
+{
+	return fOneHappened;
+}
+
+/* Function returns the boolean value that represents whether a feature happened.*/
+bool Room::getFeatureTwoHap()
+{
+	return fTwoHappened;
+}
+
+/* Function adds an item to the vector of items in the room. It receives a Player instance
+* in case the player wishes to drop an item from their inventory into the room. */
+void Room::addItem(Item* newItem, Player* user, int number)
+{
+	if (number == 0)
+	{
+		items.push_back(newItem);
+		newItem->setLocation(name);
+	}
+	else
+	{
+		items.push_back(newItem);
+
+		//Call Player function to remove item from player's inventory
+	}
+}
+
+/* Function removes an item from the vector of items in the room and adds it to the player's inventory.*/
+void Room::removeItem(Item* removeItem, Player* user)
+{
+	if (items.size() > 0)
+	{
+		for (unsigned int i = 0; i < items.size(); ++i)
+		{
+			if (items[i]->getName() == removeItem->getName())
+			{
+				user->setInventory(removeItem);
+				items.erase(items.begin() + i);
+				removeItem->setLocation("player's inventory");
+			}
+		}
+	}
+}
+
+/* Function displays the items currently in the room.*/
+void Room::itemsInRoom()
+{
+	if (items.size() > 0)
+	{
+		std::cout << "The current items in the " << getName() << " are: ";
+		for (unsigned int i = 0; i < items.size(); i++)
+		{
+			if (i != (items.size() - 1))
+			{
+				std::cout << items[i]->getName() << ", ";
+			}
+			else
+			{
+				std::cout << items[i]->getName() << std::endl;
+				std::cout << std::endl;
+			}
+		}
+	}
+	else
+	{
+		std::cout << "There are no items to pick up in the room." << std::endl;
+	}
+}
+
+/* Function displays either the short or long description based off a boolean value.*/
+void Room::displayDescrip()
+{
+	if (roomEntered == 0)
+	{
+		std::cout << longDes << std::endl;
+	}
+	else
+	{
+		std::cout << shortDes << std::endl;
+	}
+	++roomEntered;
+}
+
+/* Function displays the exit descriptions based on whether it is the first time the user entered the room.*/
+void Room::displayExits()
+{
+	if (roomEntered == 1)
+	{
+		std::cout << exitLong << std::endl;
+	}
+	else
+	{
+		std::cout << exitShort << std::endl;
+	}
+}
