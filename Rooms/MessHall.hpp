efc5ac8d4a6953d8f145386e4c9c3b7505ebe832
ryanalcorn88 <39017022+ryanalcorn88@users.noreply.github.com>
2020-01-31 18:34:21 -0800

    Create MessHall.hpp

---
+++ Rooms/MessHall.hpp
@@ -0,0 +1,29 @@
+/* Project Name: Capstone Project
+   Class: CS 467
+   Team: Team Thing- Ryan Alcorn, Joel Alfaro, Neil Gayeta
+   File: MessHall.hpp
+   Description: Class header for MessHall room.*/
+
+#pragma once
+
+#include "Room.hpp"
+
+class MessHall : public Room {
+public:
+	std::string getName();
+	std::string getLongDescrip();
+	std::string getShortDescrip();
+	std::string getExitLong();
+	std::string getExitShort();
+	bool getFeatureOneHap();
+	bool getFeatureTwoHap();
+	int featureOne(Player*);
+	int featureTwo(Player*);
+	void setRoom();
+	void itemsInRoom();
+	void addItem(Item*, Player*, int);
+	void removeItem(Item*, Player*);
+	void displayDescrip();
+	void displayExits();
+	Room* changeRooms(std::vector<Room*>, std::string);
+};
