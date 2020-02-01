4d21c42a5a8cb51228b6cd3a0bf5c7968a85d7cb
ryanalcorn88 <39017022+ryanalcorn88@users.noreply.github.com>
2020-01-31 17:49:13 -0800

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -7,7 +7,7 @@ Player::Player()
 	victory = 0;
 	std::vector<Item> inventory;
 	//Room* currentLocation = NULL;
-	gameEnd=0;
+	gameEnd = 0;
 
 }
 
@@ -23,131 +23,131 @@ void Player::useItem(std::string item)
 
 void Player::setVictory(int victory)
 {
-this->victory = victory;
+	this->victory = victory;
 }
 
 int Player::getVictory()
 {
-return victory;
+	return victory;
 }
 
-void Player::setGrit(Item powerup)
-	{
+void Player::setGrit(Item* powerup)
+{
 	/*Call to item class.  Get data member for item.  And then compare with below.*/
-		std::string type = powerup.getName();
+	std::string type = powerup->getName();
 
-		if (type == "Vodka")
-		{
-			grit = grit + 5;
-			return;
-		}
-
-		if (type == "Gin")
-		{
-			grit = grit + 4;
-			return;
-		}
-
-		if (type == "Whisky")
-		{
-			grit = grit + 3;
-			return;
-		}
-
-		if (type == "Tequila")
-		{
-			grit = grit + 2;
-			return;
-		}
-
-		if (type == "Beer")
-		{
-			grit = grit + 1;
-			return;
-		}
-
-		if (type == "Pabst")
-		{
-			grit--;
-			return;
-		}
-
-		if(type == "Mata Rata")
-		{
-			std::cout << "Not a good idea hitting the Mata Rata Tequila...That's Spanish for Rat Killer.  And apparently it is good at killing people too.";
-			setAlive(0);
-		}
-
-		if (type == "Red Herring")
-		{
-			std::cout << "You feel no effect on your grit level...How disappointing.  You feel cheated and that this was an exercise in futility." << std::endl;
-			return;
-		}
-
-		std::cout << "What are you trying to do?  You can't drink that! And you can't eat it either!" << std::endl;
+	if (type == "Vodka")
+	{
+		grit = grit + 5;
+		return;
 	}
 
-	/*Use this for grit checks in game.  Certain circumstances will cause death if it is too high or too low.*/
-	int Player::getGrit()
+	if (type == "Gin")
 	{
-		return grit;
+		grit = grit + 4;
+		return;
 	}
 
-	void Player::setAlive(int dead)
+	if (type == "Whisky")
 	{
-		alive = dead;
-
-		if (alive == 0)
-		{
-			std::cout << '\a';
-			std::cout << "Gosh darn it!  You died!\n" << std::endl;
-			std::cout << "That's it man!  Game over, man!  GAME OVER!" << std::endl;
-			exit(0);
-		}
+		grit = grit + 3;
+		return;
 	}
 
-	int Player::getAlive()
+	if (type == "Tequila")
 	{
+		grit = grit + 2;
+		return;
+	}
+
+	if (type == "Beer")
+	{
+		grit = grit + 1;
+		return;
+	}
+
+	if (type == "Pabst")
+	{
+		grit--;
+		return;
+	}
+
+	if (type == "Mata Rata")
+	{
+		std::cout << "Not a good idea hitting the Mata Rata Tequila...That's Spanish for Rat Killer.  And apparently it is good at killing people too.";
+		setAlive(0);
+	}
+
+	if (type == "Red Herring")
+	{
+		std::cout << "You feel no effect on your grit level...How disappointing.  You feel cheated and that this was an exercise in futility." << std::endl;
+		return;
+	}
+
+	std::cout << "What are you trying to do?  You can't drink that! And you can't eat it either!" << std::endl;
+}
+
+/*Use this for grit checks in game.  Certain circumstances will cause death if it is too high or too low.*/
+int Player::getGrit()
+{
+	return grit;
+}
+
+void Player::setAlive(int dead)
+{
+	alive = dead;
+
+	if (alive == 0)
+	{
+		std::cout << '\a';
+		std::cout << "Gosh darn it!  You died!\n" << std::endl;
+		std::cout << "That's it man!  Game over, man!  GAME OVER!" << std::endl;
+		exit(0);
+	}
+}
+
+int Player::getAlive()
+{
 	return alive;
-	}
+}
 
 
-	void Player::setInventory(Item newItem)
+void Player::setInventory(Item* newItem)
+{
+	inventory.push_back(newItem);
+}
+
+/*Allows player to see list of inventory.  Not the same as checkInventory*/
+void Player::getInventory()
+{
+	for (int i = 0; i < inventory.size(); i++)
 	{
-		inventory.push_back(newItem);
+		Item element = *inventory[i];
+		Item* elementPtr = &element;
+		std::string elementName = elementPtr->getName();
+		std::cout << elementName << std::endl;
 	}
+}
 
-	/*Allows player to see list of inventory.  Not the same as checkInventory*/
-	void Player::getInventory()
+/*Used to check if player has necessary item to pass test.*/
+bool Player::checkInventory(std::string itemNeeded)
+{
+	for (int i = 0; i < inventory.size(); i++)
 	{
-		for (int i = 0; i < inventory.size(); i++)
+		Item element = *inventory[i];
+		Item* elementPtr = &element;
+		std::string elementName = elementPtr->getName();
+
+		if (elementName == itemNeeded)
 		{
-			Item element = inventory[i];
-			Item* elementPtr = &element;
-			std::string elementName=elementPtr->getName();
-			std::cout << elementName << std::endl;
+			return 1;
 		}
+		else
+			return 0;
 	}
+}
 
-	/*Used to check if player has necessary item to pass test.*/
-	bool Player::checkInventory(std::string itemNeeded)
-	{
-		for (int i = 0; i < inventory.size(); i++)
-		{
-			Item element = inventory[i];
-			Item* elementPtr = &element;
-			std::string elementName = elementPtr->getName();
-			
-			if (elementName == itemNeeded)
-			{
-				return 1;
-			}
-			else
-				return 0;
-		}
-	}
-
-	/*Set location for player.  Starts as null but at start of game points to MacReady's quarters.*/
+/*Set location for player.  Starts as null but at start of game points to MacReady's quarters.*/
 //	void Player::setLocation(Room* newLocation)
 //	{
 //		currentLocation = newLocation;
@@ -164,23 +164,22 @@ void Player::setGrit(Item powerup)
 
 
 
-	void Player::setGameEnd(int ending)
-	{
-	gameEnd=ending;
+void Player::setGameEnd(int ending)
+{
+	gameEnd = ending;
 
-	}
-	
-
+}
 
 
-	int Player::getGameEnd()
-	{
+
+
+int Player::getGameEnd()
+{
 	return gameEnd;
-	}
+}
 
 //	Room* Player::returnLocation()
 
 //	{
 //	return currentLocation;
 //	}
-
