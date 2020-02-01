6bf5187df16d96cd62a903ce6ba2d236866042bb
ryanalcorn88 <39017022+ryanalcorn88@users.noreply.github.com>
2020-02-01 10:15:09 -0800

    Create Basement.hpp

---
+++ Rooms/Basement.hpp
@@ -0,0 +1,17 @@
+/* Project Name: Capstone Project
+   Class: CS 467
+   Team: Team Thing- Ryan Alcorn, Joel Alfaro, Neil Gayeta
+   File: Basement.hpp
+   Description: Class header for Basement room.*/
+
+#pragma once
+
+#include "Room.hpp"
+
+class Basement : public Room {
+public:
+	int featureOne(Player*);
+	int featureTwo(Player*);
+	void setRoom();
+	Room* changeRooms(std::vector<Room*>, std::string);
+}; 
