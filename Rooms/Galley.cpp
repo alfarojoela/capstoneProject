fdf1f6f30eadbb8b58fb6bd712c1e7c7b83fe1f1
ryanalcorn88 <39017022+ryanalcorn88@users.noreply.github.com>
2020-02-01 09:44:09 -0800

    Update Galley.cpp

--- Rooms/Galley.cpp
+++ Rooms/Galley.cpp
@@ -14,54 +14,12 @@ void Galley::setRoom()
 		"The room usually smells good, but today it smells entirely different. There is more of a rotten smell"
 		" in the air.\nYou sense the area where the smell is coming from. You also notice an object on the counter.\n";
 	shortDes = "You return to the galley. The area still smells terrible.\n";
-	exitLong = "The mess hall is from the direction you came. It is the area of the base where members of the"
+	exitLong = "The mess hall is from the direction you came.\nIt is the area of the base where members of the"
 		" crew ate their meals together.\n";
 	exitShort = "The mess hall is from the direction you came.\n";
 	fOneHappened = 0;
 	fTwoHappened = 0;
 	roomEntered = 0;
-}
-
-/* Function returns the name of the room. */
-std::string Galley::getName()
-{
-	return name;
-}
-
-/* Function returns the long description of the room. */
-std::string Galley::getLongDescrip()
-{
-	return longDes;
-}
-
-/* Function returns the short description of the room. */
-std::string Galley::getShortDescrip()
-{
-	return shortDes;
-}
-
-/* Function returns the long version of the exits description of the room. */
-std::string Galley::getExitLong()
-{
-	return exitLong;
-}
-
-/* Function returns the short version of the exits description of the room. */
-std::string Galley::getExitShort()
-{
-	return exitShort;
-}
-
-/* Function returns the boolean value that represents whether a feature happened.*/
-bool Galley::getFeatureOneHap()
-{
-	return fOneHappened;
-}
-
-/* Function returns the boolean value that represents whether a feature happened.*/
-bool Galley::getFeatureTwoHap()
-{
-	return fTwoHappened;
 }
 
 /* Function performs the action for the first feature of the room.*/
@@ -88,92 +46,6 @@ int Galley::featureTwo(Player* user)
 	++fTwoHappened;
 
 	return outcome;
-}
-
-/* Function adds an item to the vector of items in the room. It receives a Player instance
-* in case the player wishes to drop an item from their inventory into the room. */
-void Galley::addItem(Item* newItem, Player* user, int number)
-{
-	if (number == 0)
-	{
-		items.push_back(newItem);
-		newItem->setLocation(name);
-	}
-	else
-	{
-		items.push_back(newItem);
-
-		//Call Player function to remove item from player's inventory
-	}
-}
-
-/* Function removes an item from the vector of items in the room and adds it to the player's inventory.*/
-void Galley::removeItem(Item* removeItem, Player* user)
-{
-	if (items.size() > 0)
-	{
-		for (unsigned int i = 0; i < items.size(); ++i)
-		{
-			if (items[i]->getName() == removeItem->getName())
-			{
-				user->setInventory(removeItem);
-				items.erase(items.begin() + i);
-				removeItem->setLocation("player's inventory");
-			}
-		}
-	}
-}
-
-/* Function displays the items currently in the room.*/
-void Galley::itemsInRoom()
-{
-	if (items.size() > 0)
-	{
-		std::cout << "The current items in the " << getName() << " are: ";
-		for (unsigned int i = 0; i < items.size(); i++)
-		{
-			if (i != (items.size() - 1))
-			{
-				std::cout << items[i]->getName() << ", ";
-			}
-			else
-			{
-				std::cout << items[i]->getName() << std::endl;
-				std::cout << std::endl;
-			}
-		}
-	}
-	else
-	{
-		std::cout << "There are no items to pick up in the room." << std::endl;
-	}
-}
-
-/* Function displays either the short or long description based off a boolean value.*/
-void Galley::displayDescrip()
-{
-	if (roomEntered == 0)
-	{
-		std::cout << longDes << std::endl;
-	}
-	else
-	{
-		std::cout << shortDes << std::endl;
-	}
-	++roomEntered;
-}
-
-/* Function displays the exit descriptions based on whether it is the first time the user entered the room.*/
-void Galley::displayExits()
-{
-	if (roomEntered == 1)
-	{
-		std::cout << exitLong << std::endl;
-	}
-	else
-	{
-		std::cout << exitShort << std::endl;
-	}
 }
 
 /* Function receives the list of rooms and a phrase from the user. It then selects a room to move to based on
