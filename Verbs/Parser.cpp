c9ac494b432fe814198d8601ca3a1d3c00544a6b
Neil Gayeta <neilrg2@gmail.com>
2020-01-31 20:33:00 -0800

    Update Parser.cpp & Parser.hpp

--- Verbs/Parser.cpp
+++ Verbs/Parser.cpp
@@ -7,13 +7,16 @@
 #include "Parser.hpp"
 
 /* Function initializes the class verbs vector with all the valid verb objects */
-std::vector<Verb *> initializeActions() 
+std::vector<Verb *> initializeActions()
 {
     std::vector<Verb *> actions;
 
     actions.emplace_back(new Talk());
     actions.emplace_back(new Smell());
     actions.emplace_back(new Drink());
+    actions.emplace_back(new Drop());
+    actions.emplace_back(new Attack());
+    actions.emplace_back(new Eat());
     // TODO: MORE VERBS TO BE ADDED
 
     return actions;
@@ -22,9 +25,49 @@ std::vector<Verb *> initializeActions()
 /* Static class vector initialized with all valid verb objects */
 std::vector<Verb *> Parser::validActions = initializeActions();
 
+/* Static getter for valid action vector */
+std::vector<Verb *> Parser::getValidActions() 
+{
+    return Parser::validActions;
+}
+
+/* Function initializes the similar verbs unordered map with all valid similar verbs */
+std::unordered_map<std::string, std::vector<std::string>> initializeSimilarActions()
+{
+    std::unordered_map<std::string, std::vector<std::string>> similarActions;
+
+    auto validActions = Parser::getValidActions();
+
+    similarActions.insert( { validActions.front()->getName(), similar::getTalkVerbs() } );
+    validActions.erase(validActions.begin());
+
+    similarActions.insert( { validActions.front()->getName(), similar::getSmellVerbs() } );
+    validActions.erase(validActions.begin());
+
+    similarActions.insert( { validActions.front()->getName(), similar::getDrinkVerbs() } );
+    validActions.erase(validActions.begin());
+
+    similarActions.insert( { validActions.front()->getName(), similar::getDropVerbs() } );
+    validActions.erase(validActions.begin());
+
+    similarActions.insert( { validActions.front()->getName(), similar::getAttackVerbs() } );
+    validActions.erase(validActions.begin());
+
+    similarActions.insert( { validActions.front()->getName(), similar::getEatVerbs() } );
+    validActions.erase(validActions.begin());
+
+    // TODO: ADDING MORE SIMILAR ACTIONS
+
+    return similarActions;
+}
+
+/* Static class unordered map which contains all similar verbs that will be compared against the valid actions */
+std::unordered_map<std::string, std::vector<std::string>> Parser::similarActions = initializeSimilarActions();
+
 /* Destructor */
 Parser::~Parser()
 {
+    /* Clear vector allocation */
     std::cout << this->validActions.size() << std::endl;
     for (auto verb = validActions.begin(); verb != validActions.end(); verb++)
     {
--- Verbs/Parser.hpp
+++ Verbs/Parser.hpp
@@ -8,18 +8,30 @@
 
 #include <unordered_map>
 #include <vector>
+#include <utility>
+
+#include "SimilarVerbHelper.hpp"
 
 #include "Drink.hpp"
 #include "Smell.hpp"
 #include "Talk.hpp"
+#include "Drop.hpp"
+#include "Attack.hpp"
+#include "Eat.hpp"
 
 class Parser {
 
 private:
     static std::vector<Verb *> validActions;
+    static std::unordered_map<std::string, std::vector<std::string>> similarActions;
     // TODO: CONTINUE ADDING PARSER MEMBERS & FUNCTIONALITY
 
 public:
     ~Parser();
+    static std::vector<std::string> parseInput(std::string);
+    static std::vector<Verb *> getValidActions();
+    static std::unordered_map<std::string, std::vector<std::string>> getSimilarActions();
+
+
 
 };
