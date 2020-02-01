47e0ae477fd47e72e04f65fc6150017f0587041d
ryanalcorn88 <39017022+ryanalcorn88@users.noreply.github.com>
2020-02-01 09:47:09 -0800

    Update Macready.hpp

--- Rooms/Macready.hpp
+++ Rooms/Macready.hpp
@@ -7,24 +7,11 @@
 #pragma once
 
 #include "Room.hpp"
-#include "Player.hpp"
 
 class Macready : public Room {
 public:
-	std::string getName();
-	std::string getLongDescrip();
-	std::string getShortDescrip();
-	std::string getExitLong();
-	std::string getExitShort();
-	bool getFeatureOneHap();
-	bool getFeatureTwoHap();
 	int featureOne(Player*);
 	int featureTwo(Player*);
 	void setRoom();
-	void itemsInRoom();
-	void addItem(Item*, Player*, int);
-	void removeItem(Item*, Player*);
-	void displayDescrip();
-	void displayExits();
 	Room* changeRooms(std::vector<Room*>, std::string);
 };
