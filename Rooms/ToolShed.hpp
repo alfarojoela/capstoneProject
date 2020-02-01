9d8ebdcf955647842a5687459005e6f07cb84b88
ryanalcorn88 <39017022+ryanalcorn88@users.noreply.github.com>
2020-02-01 09:51:16 -0800

    Update ToolShed.hpp

--- Rooms/ToolShed.hpp
+++ Rooms/ToolShed.hpp
@@ -10,20 +10,8 @@
 
 class ToolShed : public Room {
 public:
-	std::string getName();
-	std::string getLongDescrip();
-	std::string getShortDescrip();
-	std::string getExitLong();
-	std::string getExitShort();
-	bool getFeatureOneHap();
-	bool getFeatureTwoHap();
 	int featureOne(Player*);
 	int featureTwo(Player*);
 	void setRoom();
-	void itemsInRoom();
-	void addItem(Item*, Player*, int);
-	void removeItem(Item*, Player*);
-	void displayDescrip();
-	void displayExits();
 	Room* changeRooms(std::vector<Room*>, std::string);
 };
