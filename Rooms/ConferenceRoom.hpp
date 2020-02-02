a29aad0d1c3eac2d85af0aa97563a0ef2fd93191
ryanalcorn88 <39017022+ryanalcorn88@users.noreply.github.com>
2020-02-01 22:43:42 -0800

    Create ConferenceRoom.hpp

---
+++ Rooms/ConferenceRoom.hpp
@@ -0,0 +1,17 @@
+/* Project Name: Capstone Project
+   Class: CS 467
+   Team: Team Thing- Ryan Alcorn, Joel Alfaro, Neil Gayeta
+   File: ConferenceRoom.hpp
+   Description: Class header for ConferenceRoom room.*/
+
+#pragma once
+
+#include "Room.hpp"
+
+class ConferenceRoom : public Room {
+public:
+	int featureOne(Player*);
+	int featureTwo(Player*);
+	void setRoom();
+	Room* changeRooms(std::vector<Room*>, std::string);
+};
