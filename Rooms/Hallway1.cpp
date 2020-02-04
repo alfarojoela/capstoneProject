bbff42805b32b6725f646345d573f1103f554298
ryanalcorn88 <39017022+ryanalcorn88@users.noreply.github.com>
2020-02-03 16:56:33 -0800

    Update Hallway1.cpp
    Added a rough draft of feature one and two

--- Rooms/Hallway1.cpp
+++ Rooms/Hallway1.cpp
@@ -14,9 +14,9 @@ void Hallway1::setRoom()
 		"There appears to be nobody around right now. There is a terrible smell emanating from the room.\n"
 		"Along with the smell, the lights are blinking on and off. The room wasn't like this yesterday...\n";
 	shortDes = "You enter a hallway. This hallway is connected to many rooms. The lights are blinking on and off. On and off.\n";
-	exitLong = "This room is connected to many other rooms at the base.\nThis includes the radio room, equipment room,"
+	exitLong = "This room is connected to many other rooms in the base.\nThis includes the radio room, equipment room,"
 		" sick bay, latrine, and research lab.\n";
-	exitShort = "There are five rooms connected to this room.\n";
+	exitShort = "There are five rooms connected to this hallway.\n";
 	fOneHappened = 0;
 	fTwoHappened = 0;
 	roomEntered = 0;
@@ -27,7 +27,10 @@ int Hallway1::featureOne(Player* user)
 {
 	int outcome = 0;
 
-	std::cout << "Description of the smell of blood.\n" << std::endl;
+	std::cout << "You decide to check out the blood. The room reeks of an unfamiliar smell. It is not a pleasant smell.\n"
+		"The blood is scattered across the wall through the hallway. It looks like a Picasso painting.\n"
+		"Or even something out of one of the those horror films. Like the one's that Carpenter guy used to direct.\n"
+		"It is a disturbing sight. Something big went down in here...\n"<< std::endl;
 
 	++fOneHappened;
 
@@ -39,7 +42,11 @@ int Hallway1::featureTwo(Player* user)
 {
 	int outcome = 0;
 
-	std::cout << "Event with light blinking on and off." << std::endl;
+	std::cout << "You decide to check out the lights situation. The switch is on the other side of the hallway.\n"
+		"You begin to make your way across the hallway. Each time the hallway goes dark, you get an uneasy feeling.\n"
+		"Or maybe you're just hungover.\nYou make it to the light switch. It appears to be in functional condition.\n"
+		"All of the sudden something rushes past your leg. It felt like a dog, but at the same time unnatural.\n"
+		"This place is starting to give you the creeps.\n"<< std::endl;
 
 	++fTwoHappened;
 
