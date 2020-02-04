b33337b860a3fe3d7bd68033a6a7ffe1ddbe1ffe
ryanalcorn88 <39017022+ryanalcorn88@users.noreply.github.com>
2020-02-03 16:46:00 -0800

    Update Macready.cpp
    Added changes to text layout

--- Rooms/Macready.cpp
+++ Rooms/Macready.cpp
@@ -14,7 +14,7 @@ void Macready::setRoom()
 		"You are not in your quarters. This is MacReady's room. It doesn't look like he's around though.\n"
 		"You could search around for more booze. You also notice a computer in the room with a chess game in progress.\n";
 	shortDes = "You return to the MacReady's quarters. There's still no booze in here.\n";
-	exitLong = "There is a hallway outside this room that leads to other rooms on the base.\nThe base is eerily quiet right now.\n";
+	exitLong = "There is a hallway outside this room that leads to other rooms in the base.\nThe base is eerily quiet right now.\n";
 	exitShort = "There is a hallway outside this room.\n";
 	fOneHappened = 0;
 	fTwoHappened = 0;
@@ -47,18 +47,17 @@ int Macready::featureTwo(Player* user)
 	std::cout << "The computer screen is one of the main sources of light in the room. There is a chess game in progress.\n"
 		"The game looks to be about midway through. The taken pieces on both sides seem pretty easily matched.\n"
 		"Analysing the screen you see only two viable options. You could take a risk and move you queen down to check.\n"
-		"You could also move your knight to block and bait out any incoming attacks towards your king.\n\n"
-		"Which do you choose?\n"
+		"You could also move your knight to block and bait out any incoming attacks towards your king.\n"
+		"Which do you choose?\n\n"
 		"1. Check the bot's king\n"
 		"2. Protect your king" << std::endl;
 		std::cin >> choice;
 
 		while (response == "incorrect")
 		{
-			std::cout << std::endl;
-
 			if (choice == "1")
 			{
+				std::cout << std::endl;
 				std::cout << "You decide to check the king. The best defense is a good offense you think.\n"
 					"The computer's queen moves to a new position and puts your king in check and then checkmate.\n"
 					"The game flashes 'You lose!' over and over again. Stunned by this sudden defeat you get up to continue your search.\n"
@@ -68,6 +67,7 @@ int Macready::featureTwo(Player* user)
 			}
 			else if (choice == "2")
 			{
+				std::cout << std::endl;
 				std::cout << "You decide to protect your king. The computer foolishly moves their queen to take your knight.\n"
 					"You end up taking their most valuable piece with the bait you set up. A couple of turns later you defeat the computer.\n"
 					"You think to yourself, 'I thought MacReady said this game was hard?' and walk away to continue your search." << std::endl;
@@ -80,8 +80,6 @@ int Macready::featureTwo(Player* user)
 				std::cin >> choice;
 			}
 		}
-
-		std::cout << std::endl;
 
 		++fTwoHappened;
 
