654b0a9eb07cb31fb120c8975276b486696a6e8d
ryanalcorn88 <39017022+ryanalcorn88@users.noreply.github.com>
2020-02-04 15:40:37 -0800

    Added features to the room
    Added features to the room

--- Rooms/Latrine.cpp
+++ Rooms/Latrine.cpp
@@ -10,7 +10,7 @@
 void Latrine::setRoom()
 {
 	name = "latrine";
-	longDes = "You enter the latrine. This is the area of the base where that contains the restroom.\n"
+	longDes = "You enter the latrine. This is the area of the base that contains the restroom.\n"
 		"This room usually does not smell good. It's not a room you want to stay in for long,\nbut you think you remember booze being in here."
 		" You notice some toilet paper in an odd location in the room.\nYou also have the need to use the restroom.\n";
 	shortDes = "You return to the latrine. It's a bathroom.\n";
@@ -25,8 +25,17 @@ void Latrine::setRoom()
 int Latrine::featureOne(Player* user)
 {
 	int outcome = 0;
+	int index = 0;
 
-	std::cout << "Player inspects the toilet paper." << std::endl;
+	std::cout << "You decide to inspect the toilet paper. It is sprawled on the ground in an abnormal way.\n"
+		"Either someone's pulling a prank or something's going on.\n"
+		"The writing on the toilet paper appears to be in charcoal. This is weird.\n"
+		"There are numbers written on here. 5...3...9...2.\n"
+		"Someone wrote the code to the flamethrower locker on here. Odd...\n"<< std::endl;
+
+	index = findItemIndex("toilet paper");
+
+	removeItem(items[index], user);
 
 	++fOneHappened;
 
@@ -37,34 +46,85 @@ int Latrine::featureOne(Player* user)
 int Latrine::featureTwo(Player* user)
 {
 	int outcome = 0;
+	int number = 0;
+	std::string choice = "";
+	std::string response = "incorrect";
 
-	std::cout << "Player uses the bathroom." << std::endl;
+	std::cout << "You decide to use the restroom. It was a long night.\n"
+		"You begin to hear splashing sounds which causes you to have an uneasy feeling.\n"
+		"You finish using the restroom and begin to walk away.\n"
+		"As you look back all of the sudden something jumps at you!\n"
+		"What do you do?\n\n"
+		"1. Attack it with your fists\n"
+		"2. Attack it with a weapon\n"
+		"3. Attempt to dodge the attack\n" << std::endl;
+	std::cin >> choice;
+
+	while (response == "incorrect")
+	{
+		if (choice == "1")
+		{
+			std::cout << std::endl;
+			std::cout << "You attack it with your fists. As the thing flies at you, you notice how small it is.\n"
+				"Easy enough to punch. You send it flying back into the wall!\n"
+				"Whatever that was, it isn't getting up for a while.\n" << std::endl;
+
+			response = "correct";
+		}
+		else if (choice == "2")
+		{
+			std::cout << std::endl;
+			//Add function to check if user has specific items to protect themselves
+			if (user->checkInventory("gun") || user->checkInventory("flamethrower") || user->checkInventory("axe"))
+			{
+				std::cout << "You attack the creature with a weapon. It stood no chance!\n"
+					"Pieces of the creature scatter across the room. Is this real or are you still dreaming in a drunken stupor?\n" << std::endl;
+
+				//Call function to remove item from inventory or set its singleUse to 1
+
+				response = "correct";
+			}
+			else
+			{
+				std::cout << "You do not have a weapon to fend off the creature!" << std::endl;
+				choice = "3";
+			}
+		}
+
+		if (choice == "3")
+		{
+			std::cout << std::endl;
+			std::cout << "You attempt to dodge the incoming attack!" << std::endl;
+			std::cout << std::endl;
+
+			number = rand() % 100 + 1;
+
+			if (number > 50)
+			{
+				std::cout << "You successfully dodge the attack! The creature flies by you while shrieking.\n"
+					"It lands on the ground hard and appears not to be moving.\n"<< std::endl;
+			}
+			else
+			{
+				std::cout << "You are too slow! The creature bites you in the shoulder. You feel an intense pain.\n"
+					"Reflexively, you grab it and fling it against the wall. The creature appears to stop moving.\n"
+					"You lose one health!\n" << std::endl;
+
+				//Outcome set to 1 to call function to lose health
+				outcome = 1;
+			}
+
+			response = "correct";
+		}
+
+		if (choice != "1" && choice != "2" && choice != "3")
+		{
+			std::cout << "You have picked an incorrect choice. Please chose again: ";
+			std::cin >> choice;
+		}
+	}
 
 	++fTwoHappened;
 
 	return outcome;
-}
-
-/* Function receives the list of rooms and a phrase from the user. It then selects a room to move to based on
-* the phrase. It returns a pointer to that room to main so the game can move to that room. */
-Room* Latrine::changeRooms(std::vector<Room*> rooms, std::string phrase)
-{
-	Room* nextRoom = NULL;
-	std::string roomName = "";
-
-	if (phrase == "go back" || phrase == "back" || phrase == "go hallway" || phrase == "hallway")
-	{
-		roomName = "hallway1";
-	}
-
-	for (int x = 0; x < 15; ++x)
-	{
-		if (rooms[x]->getName() == roomName)
-		{
-			nextRoom = rooms[x];
-			x = 15;
-		}
-	}
-
-	return nextRoom;
 }
