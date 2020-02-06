d8be27406cfeba2ff8c161a656649259683f027d
ryanalcorn88 <39017022+ryanalcorn88@users.noreply.github.com>
2020-02-05 18:34:49 -0800

    Added features to the room

--- Rooms/ConferenceRoom.cpp
+++ Rooms/ConferenceRoom.cpp
@@ -25,8 +25,68 @@ void ConferenceRoom::setRoom()
 int ConferenceRoom::featureOne(Player* user)
 {
 	int outcome = 0;
+	int counter = 0;
+	std::string choice = "1";
+	std::string response = "incorrect";
 
-	std::cout << "Drink alcohol. Possibly drinking too much." << std::endl;
+	std::cout << "You hurry towards the bottle sitting in the middle of the table.\n"
+		"You take a swig.\n" << std::endl;
+
+	//While loop runs until the player decides to exit or they take 8 drinks
+	while (response == "incorrect" && counter < 8)
+	{
+		//If the player chose 1, the question is asked again.
+		if (choice == "1")
+		{
+			std::cout << "Keep drinking?\n"
+				"1. Yes\n"
+				"2. No" << std::endl;
+			std::cin >> choice;
+
+			++counter;
+		}
+
+		if (choice == "1")
+		{
+			std::cout << std::endl;
+			std::cout << "You take another drink." << std::endl;
+		}
+		else if (choice == "2")
+		{
+			std::cout << std::endl;
+			std::cout << "You decide you've had enough." << std::endl;
+
+			response = "correct";
+		}
+		else
+		{
+			std::cout << "You have picked an incorrect choice. Please chose again: ";
+			std::cin >> choice;
+		}
+
+		//Lines print out based off of how many drinks the player has had.
+		if (counter == 3)
+		{
+			std::cout << "You feel buzzed now. Getting back to the same level as last night." << std::endl;
+		}
+
+		if (counter == 6)
+		{
+			std::cout << "You feel pretty drunk. Things are getting slower." << std::endl;
+		}
+
+		if (counter == 8)
+		{
+			std::cout << "You pass out.\n"
+				"When you wake up, something is standing over you.\n"
+				"It looks human, but it's face looks like it's shrieking. There is no noise coming out though.\n"
+				"It reaches its hand towards your face! That's the last thing you see." << std::endl;
+
+			//Player drinks too much and dies causing outcome to be set to two
+			outcome = 2;
+		}
+	}
+	std::cout << std::endl;
 
 	++fOneHappened;
 
@@ -45,15 +105,14 @@ int ConferenceRoom::featureTwo(Player* user)
 		"There are three tapes to choose from. Which one do you choose?\n\n"
 		"1. Tape One\n"
 		"2. Tape Two\n"
-		"3. Tape Three\n" << std::endl;
+		"3. Tape Three" << std::endl;
 	std::cin >> choice;
 
 	while (response == "incorrect")
 	{
-		std::cout << std::endl;
-
 		if (choice == "1")
 		{
+			std::cout << std::endl;
 			std::cout << "You insert the first tape into the machine. It begins to start up.\n"
 				"It starts towards the end of one of the comedy movies that were in the inventory on the base.\n"
 				"You decide to take twenty and finish the film. That movie is hilarious!\n"
@@ -63,6 +122,7 @@ int ConferenceRoom::featureTwo(Player* user)
 		}
 		else if (choice == "2")
 		{
+			std::cout << std::endl;
 			std::cout << "You insert the second tape into the machine. It begins to start up.\n"
 				"It starts up at the beginning of the tape. It appears to be a recording of birds that naturally inhabit this area.\n"
 				"You think to yourself 'This is boring' and turn off the projector.\n"
@@ -72,6 +132,7 @@ int ConferenceRoom::featureTwo(Player* user)
 		}
 		else if (choice == "3")
 		{
+			std::cout << std::endl;
 			std::cout << "You insert the third tape into the machine. It begins to start up.\n"
 				"There is only black screen for about two minutes. All of the sudden you hear a crumpling noise.\n"
 				"You check the machine. It appears that the tape malfunctioned and ruined the machine in the process.\n" << std::endl;
@@ -88,28 +149,4 @@ int ConferenceRoom::featureTwo(Player* user)
 	++fTwoHappened;
 
 	return outcome;
-}
-
-/* Function receives the list of rooms and a phrase from the user. It then selects a room to move to based on
-* the phrase. It returns a pointer to that room to main so the game can move to that room. */
-Room* ConferenceRoom::changeRooms(std::vector<Room*> rooms, std::string phrase)
-{
-	Room* nextRoom = NULL;
-	std::string roomName = "";
-
-	if (phrase == "go back" || phrase == "back" || phrase == "go hallway" || phrase == "hallway")
-	{
-		roomName = "hallway2";
-	}
-
-	for (int x = 0; x < 15; ++x)
-	{
-		if (rooms[x]->getName() == roomName)
-		{
-			nextRoom = rooms[x];
-			x = 15;
-		}
-	}
-
-	return nextRoom;
 }
