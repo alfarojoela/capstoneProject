624211d4762cde02917a409f9a2b19c9983f4c04
ryanalcorn88 <39017022+ryanalcorn88@users.noreply.github.com>
2020-02-06 13:54:47 -0800

    Added actions to the features

--- Rooms/MessHall.cpp
+++ Rooms/MessHall.cpp
@@ -15,61 +15,126 @@ void MessHall::setRoom()
 		"You notice what looks like an untouched meal on one of the tables. There's also a bottle of booze on another table.\n";
 	shortDes = "You return to the mess hall. The room where the crew ate their meals together.\n";
 	exitLong = "A hallway which connects to many rooms in the base including the conference room and garage is east of you.\n"
-		"The galley is also south of your location.";
+		"The galley is also south of your location.\n";
 	exitShort = "There is a hallway to the east and the galley to the south.\n";
 	fOneHappened = 0;
 	fTwoHappened = 0;
 	roomEntered = 0;
 }
 
-/* Function performs the action for the first feature of the room.*/
+/* Function performs the action for the first feature of the room where the player can get food to gain health.*/
 int MessHall::featureOne(Player* user)
 {
 	int outcome = 0;
+	std::string choice = "";
+	std::string response = "incorrect";
 
-	std::cout << "Player eats the food in the room." << std::endl;
+	std::cout << "You make your way over to the food. It's a plate of spaghetti!\n"
+		"You are extremely hungry right now.\n"
+		"What do you do?\n\n"
+		"1. Eat the food\n"
+		"2. Walk away"<< std::endl;
+	std::cin >> choice;
+
+	while (response == "incorrect")
+	{
+		if (choice == "1")
+		{
+			std::cout << std::endl;
+			std::cout << "You decide to eat the spaghetti. After taking the first bite you can tell it has been sitting out for a bit.\n"
+				"It's still slightly warm and helps with the hangover. You take 15 minutes to eat the plate of spaghetti.\n"
+				"You gain one health!"<< std::endl;
+
+			//Player gains a health through function call or by outcome being set to 3
+			outcome = 3;
+
+			response = "correct";
+		}
+		else if (choice == "2")
+		{
+			std::cout << std::endl;
+			std::cout << "You decide to not eat the food. It isn't booze, who cares?" << std::endl;
+
+			response = "correct";
+		}
+		else
+		{
+			std::cout << "You have picked an incorrect choice. Please chose again: ";
+			std::cin >> choice;
+		}
+	}
+	std::cout << std::endl;
 
 	++fOneHappened;
 
 	return outcome;
 }
 
-/* Function performs the action for the second feature of the room.*/
+/* Function performs the action for the second feature of the room where the player could potentally gain a bottle of gin or get hurt.*/
 int MessHall::featureTwo(Player* user)
 {
 	int outcome = 0;
+	int number = 0;
+	std::string choice = "";
+	std::string response = "incorrect";
 
-	std::cout << "Player finds the empty booze bottle. Eats angry." << std::endl;
+	std::cout << "You approach the bottle of booze. As you get closer you notice that it is empty.\n"
+		"This feels you with anger.\n"
+		"What do you do?\n\n"
+		"1. Throw the bottle against the wall\n"
+		"2. Decide it's not worth it and walk away"<< std::endl;
+	std::cin >> choice;
+
+	while (response == "incorrect")
+	{
+		if (choice == "1")
+		{
+			std::cout << std::endl;
+			std::cout << "You are so angry! You grab the top of the bottle." << std::endl;
+
+			//Random chance the player will get hurt or obtain an item
+			number = rand() % 100 + 1;
+
+			if (number > 50)
+			{
+				std::cout << "You chuck the bottle against the wall. It shatters and pieces fly everywhere.\n"
+					"As you are walking away, you notice a bottle of Beef Eater Gin behind a counter.\n"
+					"You pick it up. You obtain a bottle of gin!" << std::endl;
+
+				removeItem("gin", user);
+			}
+			else
+			{
+				std::cout << "You chuck the bottle against the wall. It shatters and pieces fly everywhere.\n"
+					"A piece ricochets back at you cutting your arm.\n"
+					"You lose one health!" << std::endl;
+
+				//Outcome set to 1 to call function to lose health
+				outcome = 1;
+			}
+
+			response = "correct";
+		}
+		else if (choice == "2")
+		{
+			std::cout << std::endl;
+			std::cout << "You decide to walk away and not take your anger out on a random bottle.\n"
+				"As you are walking away, you notice a bottle of Beef Eater Gin behind a counter.\n"
+				"You pick it up. You obtain a bottle of gin!"<< std::endl;
+
+			removeItem("gin", user);
+
+			response = "correct";
+		}
+		else
+		{
+			std::cout << "You have picked an incorrect choice. Please chose again: ";
+			std::cin >> choice;
+		}
+	}
+	std::cout << std::endl;
 
 	++fTwoHappened;
 
 	return outcome;
-}
-
-/* Function receives the list of rooms and a phrase from the user. It then selects a room to move to based on
-* the phrase. It returns a pointer to that room to main so the game can move to that room. */
-Room* MessHall::changeRooms(std::vector<Room*> rooms, std::string phrase)
-{
-	Room* nextRoom = NULL;
-	std::string roomName = "";
-
-	if (phrase == "go east" || phrase == "east" || phrase == "go hallway" || phrase == "hallway")
-	{
-		roomName = "hallway2";
-	}
-	else if (phrase == "go south" || phrase == "south" || phrase == "go galley" || phrase == "galley")
-	{
-		roomName = "galley";
-	}
-
-	for (int x = 0; x < 15; ++x)
-	{
-		if (rooms[x]->getName() == roomName)
-		{
-			nextRoom = rooms[x];
-			x = 15;
-		}
-	}
-
-	return nextRoom;
 }
