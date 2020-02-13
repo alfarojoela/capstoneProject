402889b4998ea1f474c8a9eba27553e48d634de0
Joel Alfaro <alfarojo@flip3.engr.oregonstate.edu>
2020-02-13 11:04:19 -0800

    Renamed files to avoid potential confusion with other files with same name.

---
+++ helpFunc.cpp
@@ -0,0 +1,11 @@
+#include "helpFunc.hpp"
+
+void help()
+{
+std::cout << "Game commands: " << std::endl;
+std::cout << "attack, break, drink, drop, eat," << std::endl;
+std::cout << "flee, go, help, inventory, jump," << std::endl;
+std::cout <<"look, look at, smell, take, talk, use" << std::endl;
+}
+
+
