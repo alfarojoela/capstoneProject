1172499ed4cb7dbe9784f81218e92bf428d73e7b
Joel Alfaro <alfarojo@flip2.engr.oregonstate.edu>
2020-02-13 22:09:17 -0800

    Logo now has color.

--- intro.cpp
+++ intro.cpp
@@ -1,18 +1,16 @@
+#include <stdlib.h>
+
 #include "intro.hpp"
 
 void intro()
 {
 
-	std::cout << "__________________________________________________________________________________________________________________" << std::endl;
 
-	std::cout << std::endl;
-	std::cout << "	*******  *    *  ******      *******   *    *  *******  **    *  ******  *   *                                 " << std::endl;
-	std::cout << "	   *     *    *  *     	       *       *    *     *     * *   *  *       *   *                                " << std::endl;
-	std::cout << "	   *     ******  ******        *       ******     *     *   * *  *   *** *****                                " << std::endl;
-	std::cout << "	   *     *    *  *             *       *    *     *     *    **  *    *    *                              " << std::endl;
-	std::cout << "	   *     *    *  ******        *       *    *  *******  *     *  ******    *                                  " << std::endl;
-	std::cout << "__________________________________________________________________________________________________________________" << std::endl;
-
+	std::cout << "\033[1;41m*******  *    *  ******      *******   *    *  *******  **    *  ******  *   *\033[0m\n";
+	std::cout << "\033[1;41m   *     *    *  *             *       *    *     *     * *   *  *       *   *\033[0m\n";
+	std::cout << "\033[1;41m   *     ******  ******        *       ******     *     *   * *  *   *** *****\033[0m\n";
+	std::cout << "\033[1;41m   *     *    *  *             *       *    *     *     *    **  *    *    *  \033[0m\n";
+	std::cout << "\033[1;41m   *     *    *  ******        *       *    *  *******  *     *  ******    *  \033[0m\n";
 	std::cout << "Welcome to 'THE THINGY' a text-based horror/adventure game inspired by the John Carpenter film 'The Thing' from 1982." << std::endl;
 	std::cout << "It's not inspired by the 2011 cash grab. " << std::endl;
 }
