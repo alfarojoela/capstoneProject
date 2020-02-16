1646598752a09a2077af6077561d2a1138b5c10f
Joel Alfaro <alfarojo@flip2.engr.oregonstate.edu>
2020-02-16 13:25:51 -0800

    added common phrases section to file.

--- helpFunc.cpp
+++ helpFunc.cpp
@@ -6,6 +6,10 @@ std::cout << "Game commands: " << std::endl;
 std::cout << "attack, break, drink, drop, eat," << std::endl;
 std::cout << "flee, go, help, inventory, jump," << std::endl;
 std::cout <<"look, look at, smell, take, talk, use" << std::endl;
+
+std::cout << std::endl;
+std::cout <<"Game common phrases: " <<std::endl;
+std::cout << "'look for booze' - this phrases causes the player to check any items that can be taken from the room inventory." << std::endl;
 }
 
 
