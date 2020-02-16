46475f54be5bf6f7a2ace2cae0c4785bab816dc7
Joel Alfaro <alfarojo@flip2.engr.oregonstate.edu>
2020-02-16 12:02:50 -0800

    Changed line 15.  Made floor a more prominent feature for the player.

--- Rooms/Hallway1.cpp
+++ Rooms/Hallway1.cpp
@@ -12,7 +12,7 @@ void Hallway1::setRoom()
 	name = "hallway1";
 	longDes = "You enter a hallway. This hallway is connected to many of the important rooms inside the base.\n"
 		"There appears to be nobody around right now. There is a terrible smell emanating from the room.\n"
-		"Along with the smell, the lights are blinking on and off. The room wasn't like this yesterday...\n";
+		"You notice the floor has a trail of red streaks on it.  The lights are blinking on and off. The room wasn't like this yesterday...\n";
 	shortDes = "You enter a hallway. This hallway is connected to many rooms. The lights are blinking on and off. On and off.\n";
 	exitLong = "This room is connected to many other rooms in the base.\nThis includes the radio room, equipment room,"
 		" sick bay, latrine, and research lab.\n";
