104659371effba8eb0f3038bc858e0b74c195d0a
Joel Alfaro <alfarojo@flip1.engr.oregonstate.edu>
2020-03-05 15:09:48 -0800

    eat function moved to own file.

---
+++ eatFunc.cpp
@@ -0,0 +1,15 @@
+#include "eatFunc.hpp"
+
+void eat(std::string commands[3], Room* &playerLocation, Player* &playerPtr, std::vector <Room*> &roomList, int roomNumber)
+{
+	std::string food = commands[2];
+	/*bool check = playerPtr->checkInventory(food);*/
+	if (playerPtr->checkInventory(food) == 1)
+	{
+		Item redherring("redherring");
+		playerPtr->setGrit("redherring");
+	}
+	else
+		std::cout << "Sorry.  You can't eat what you don't have in your inventory." << std::endl;
+}
+
