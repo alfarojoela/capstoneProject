aeb5a550e7353b79040656a6ff0330635bed95e8
Joel Alfaro <alfarojo@flip1.engr.oregonstate.edu>
2020-03-05 15:11:09 -0800

    drink function in own file.

---
+++ drinkFunc.cpp
@@ -0,0 +1,16 @@
+#include "drinkFunc.hpp"
+
+void drink(std::string commands[3], Room* &playerLocation, Player* &playerPtr, std::vector <Room*> &roomList, int roomNumber)
+{
+	std::string beverage = commands[2];
+	/*bool check = playerPtr->checkInventory(food);*/
+	if (playerPtr->checkInventory(beverage) == 1)
+	{
+		playerPtr->setGrit(beverage);
+		return;
+	}
+
+	std::cout << "Sorry.  You can't drink what you don't have in your inventory." << std::endl;
+	return;
+}
+
