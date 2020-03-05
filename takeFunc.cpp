e7ce7fb2f2d2f0320fddbeb65d71467880d96ff9
Joel Alfaro <alfarojo@flip1.engr.oregonstate.edu>
2020-03-05 15:09:24 -0800

    take function moved to own file.

---
+++ takeFunc.cpp
@@ -0,0 +1,29 @@
+#include "takeFunc.hpp"
+
+/*Gets a string out of the commands vector.  Sends the string to the room removeItem function with the player pointer.  Room function handles the transition of inventory to player inventory and then deletes the appropriate cell.*/
+void take(std::string commands[3], Room* &playerLocation, Player* &playerPtr, std::vector <Room*> &roomList, int roomNumber)
+{
+	std::string itemToTake = commands[2];
+	if (playerLocation->checkItem(itemToTake) != 999)
+	{
+		std::cout << "'Yoinks!'you say.  Just like Shaggy from Scooby Doo.  You found the " << itemToTake << std::endl;
+		std::cout << "You're taking it with you." << std::endl;
+		playerLocation->removeItem(itemToTake, playerPtr);
+		return;
+	}
+
+	if (playerLocation->checkItem(itemToTake) == 999 && playerLocation->getName() == "macready")
+	{
+		std::cout << "You can't do that." << std::endl;
+		std::cout << "Besides, it's not a good idea trying to take Macready's stuff without asking.  You're liable to get on his wrongside.  And you don't wanna be on his wrongside." << std::endl;
+		return;
+	}
+
+	if (playerLocation->checkItem(itemToTake) == 999)
+	{
+		std::cout << "You can't take that." << std::endl;
+		return;
+	}
+
+}
+
