bcd91186b69c38f84d42e3cdab9e519d6e6ef6fe
Joel Alfaro <alfarojo@flip2.engr.oregonstate.edu>
2020-03-04 20:11:40 -0800

    Added new command 'check blood'

--- helpFunc.cpp
+++ helpFunc.cpp
@@ -3,13 +3,14 @@
 void help()
 {
 std::cout << "Game commands: " << std::endl;
-std::cout << "attack, break, drink, drop, eat," << std::endl;
+std::cout << "attack, break, check blood, drink, drop, eat," << std::endl;
 std::cout << "flee, go, help, inventory, jump," << std::endl;
 std::cout <<"look, look at, smell, take, talk, use" << std::endl;
 
 std::cout << std::endl;
 std::cout <<"Game common phrases: " <<std::endl;
 std::cout << "'look for booze' - this phrases causes the player to check any items that can be taken from the room inventory." << std::endl;
+std::cout << "'check blood'" << std::endl;
+
+
 }
-
-
