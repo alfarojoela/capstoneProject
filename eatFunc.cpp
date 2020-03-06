65379150b2910d2dbb9968757c127879707f472d
Joel Alfaro <alfarojo@flip2.engr.oregonstate.edu>
2020-03-06 00:01:59 -0800

    Corrected issue with eat function.

--- eatFunc.cpp
+++ eatFunc.cpp
@@ -6,10 +6,11 @@ void eat(std::string commands[3], Room* &playerLocation, Player* &playerPtr, std
 	/*bool check = playerPtr->checkInventory(food);*/
 	if (playerPtr->checkInventory(food) == 1)
 	{
-		Item redherring("redherring");
-		playerPtr->setGrit("redherring");
+		Item redherring("red herring");
+		playerPtr->setGrit("red herring");
 	}
 	else
 		std::cout << "Sorry.  You can't eat what you don't have in your inventory." << std::endl;
 }
+
 
