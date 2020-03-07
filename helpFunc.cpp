1fda771d14bc382d20d0e19bb8d367a4d24ab18a
Joel Alfaro <alfarojo@flip2.engr.oregonstate.edu>
2020-03-06 17:19:18 -0800

    Revised helpFunc

--- helpFunc.cpp
+++ helpFunc.cpp
@@ -2,15 +2,17 @@
 
 void help()
 {
-std::cout << "Game commands: " << std::endl;
+
+std::cout <<"---------------------------------------------------------------------------------------" <<std::endl;
+std::cout << "In-Game Commands: " << std::endl;
 std::cout << "attack, break, check blood, drink, drop, eat," << std::endl;
 std::cout << "flee, go, help, inventory, jump," << std::endl;
-std::cout <<"look, look at, smell, take, talk, use" << std::endl;
+std::cout <<"look, look at, look for booze, smell, take, talk, use" << std::endl;
 
 std::cout << std::endl;
-std::cout <<"Game common phrases: " <<std::endl;
-std::cout << "'look for booze' - this phrases causes the player to check any items that can be taken from the room inventory." << std::endl;
-std::cout << "'check blood'" << std::endl;
+std::cout << "Game save/load commands:" << std::endl;
+std::cout << "savegame, loadgame" << std::endl;
 
-
+std::cout <<"---------------------------------------------------------------------------------------" <<std::endl;
 }
+
