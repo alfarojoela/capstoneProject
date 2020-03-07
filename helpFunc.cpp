9ff6a66aad50f098182ec27a6b5037d06a73979b
Joel Alfaro <alfarojo@flip2.engr.oregonstate.edu>
2020-03-06 21:17:19 -0800

    Revised helpFunc with additional commands.

--- helpFunc.cpp
+++ helpFunc.cpp
@@ -3,16 +3,16 @@
 void help()
 {
 
-std::cout <<"---------------------------------------------------------------------------------------" <<std::endl;
-std::cout << "In-Game Commands: " << std::endl;
+std::cout << "------------------------------------------------------------------------------------------------------------" << std::endl;
+std::cout << "Game Commands: " << std::endl;
 std::cout << "attack, break, check blood, drink, drop, eat," << std::endl;
 std::cout << "flee, go, help, inventory, jump," << std::endl;
-std::cout <<"look, look at, look for booze, smell, take, talk, use" << std::endl;
+std::cout <<"look, look at, look for booze, map, smell, take, talk, use" << std::endl;
 
 std::cout << std::endl;
 std::cout << "Game save/load commands:" << std::endl;
 std::cout << "savegame, loadgame" << std::endl;
+std::cout << "------------------------------------------------------------------------------------------------------------" << std::endl;
 
-std::cout <<"---------------------------------------------------------------------------------------" <<std::endl;
 }
 
